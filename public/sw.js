cc2f5d4ae3179c0b8024ee39f0be33bcbb3a5334
Scottish74 <franckscotton@gmail.com>
2026-04-03 12:00:57 +0200

    fix: service worker Android install

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,3 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-));
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
